0f0d684a2cf64a0f5494603b9edb8adc0d893039
caribou <kingdomflo16@gmail.com>
2016-11-03 17:12:48 +0100

    nothing special

--- app/src/main/java/com/caribou/yaweapp/fragment/HomeFragment.java
+++ app/src/main/java/com/caribou/yaweapp/fragment/HomeFragment.java
@@ -3,12 +3,14 @@ package com.caribou.yaweapp.fragment;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -26,6 +28,9 @@ public class HomeFragment extends Fragment implements GetAsyncTask.GetAsyncTaskC
     // TODO add message guild and list of usefull url
 
     TextView tvGuildMessage;
+    Button btForum;
+    Button btWiki;
+    Button btMetaBattle;
 
     private HomeFragmentCallback callback;
 
@@ -45,10 +50,44 @@ public class HomeFragment extends Fragment implements GetAsyncTask.GetAsyncTaskC
         View v = inflater.inflate(R.layout.fragment_home, container, false);
 
         tvGuildMessage = (TextView) v.findViewById(R.id.tv_home_guildMessage);
+
         GetAsyncTask task = new GetAsyncTask(HomeFragment.this);
         task.execute(ListOfApiUrl.getUrlGuildMessage());
 
+        btForum = (Button) v.findViewById(R.id.bt_home_forum);
+        btWiki = (Button) v.findViewById(R.id.bt_home_wiki);
+        btMetaBattle = (Button) v.findViewById(R.id.bt_home_metabattle);
+
+        btForum.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intentToWeb(ListOfApiUrl.getUrlForum());
+            }
+        });
+
+        btMetaBattle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intentToWeb(ListOfApiUrl.getUrlMetabattle());
+            }
+        });
+
+        btWiki.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                intentToWeb(ListOfApiUrl.getUrlWikiGW2());
+            }
+        });
+
         return v;
+    }
+
+    public void intentToWeb(String url){
+        Uri webpage = Uri.parse(url);
+        Intent intent = new Intent(Intent.ACTION_VIEW, webpage);
+        if (intent.resolveActivity(getActivity().getPackageManager()) != null) {
+            startActivity(intent);
+        }
     }
 
     @Override
